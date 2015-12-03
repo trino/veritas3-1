667a639e08baaaeb93f4656c7151e18df9d43c5e
trino <minesh@trinoweb.com>
2015-12-03 09:38:14 -0500

    Roy 9:35
    works with multiple languages

--- webroot/subpages/profile/page.php
+++ webroot/subpages/profile/page.php
@@ -1,70 +1,46 @@
-<!-- BEGIN PORTLET-->
-<!--<div class="portlet box green-haze">
-    <div class="portlet-title">
-        <div class="caption">
-            <i class="fa fa-briefcase"></i>Pages
-        </div>
-        -->
 <?php
- if($this->request->session()->read('debug')){ echo "<span style ='color:red;'>subpages/profile/page.php #INC123</span>";}
+if($this->request->session()->read('debug')){ echo "<span style ='color:red;'>subpages/profile/page.php #INC123</span>";}
+include_once("subpages/api.php");
+$languages = languages();
 
-
-function makepage($webroot, $tabIndex, $name, $cms, $active){
+function makepage($webroot, $tabIndex, $name, $cms, $active, $languages){
     //$cms = $This->requestAction("/pages/get_content/' . $name . '");
     $Title = ucfirst(str_replace("_", " ", $name));
 
-    echo '<!-- BEGIN FORM-->
-                                        <div class="tab-pane ' . $active . '" id="subtab_1_' . $tabIndex . '">
-                                            <div class="portlet box">
-                                                <div class="portlet-body form">
-<form action="' . $webroot . 'pages/edit/' . $name . '" method="post" class="form-horizontal form-bordered" id="' . $name . '">
-    <input type="hidden" name="languages" value="English,French" />
-    <div class="form-body">
-        <div class="form-group last">
-            <label class="col-md-2"></label>
-            <label class="col-md-5" align="CENTER">English</label>
-            <label class="col-md-5" align="CENTER">Français</label>
-        </div>
-    </div>
+    echo '<!-- BEGIN FORM--><div class="tab-pane ' . $active . '" id="subtab_1_' . $tabIndex . '"><div class="portlet box"><div class="portlet-body form">';
+    echo '<form action="' . $webroot . 'pages/edit/' . $name . '" method="post" class="form-horizontal form-bordered" id="' . $name . '">';
+    echo '<input type="hidden" name="languages" value="' . implode(",", $languages) . '" />';
 
-    <div class="form-body">
-        <div class="form-group last">
-            <label class="control-label col-md-2">' . $Title . ' Title</label>
-            <div class="col-md-5">
-                <input class="form-control" name="title" id="title-' . $name . '" value="' . ucfirst($cms->title) . '"/>
-            </div>
-             <div class="col-md-5">
-                <input class="form-control" name="titleFrench" id="titleFrench-' . $name . '" value="' . ucfirst($cms->titleFrench) . '"/>
-            </div>
-        </div>
-    </div>
-    <div class="form-body">
-        <div class="form-group last">
-            <label class="control-label col-md-2">Description</label>
-            <div class="col-md-5">
-                                                                    <textarea class="ckeditor form-control"
-                                                                              name="desc" rows="6" id="desc' . $name . '">' . $cms->desc . '</textarea>
-            </div>
-            <div class="col-md-5">
-                                                                    <textarea class="ckeditor form-control"
-                                                                              name="descFrench" rows="6" id="descFrench' . $name . '">' . $cms->descFrench . '</textarea>
-            </div>
-        </div>
-    </div>
-    <div class="form-actions" style="margin-left: -10px;margin-right: -10px;">
-        <div class="row" align="right">
-            <div class="col-md-offset-2 col-md-10">
-                <button type="submit"   class="btn btn-primary"  >
-                    Save Changes
-                </button>
-                <button type="button" class="btn default" style="margin-right: 8px;">Cancel
-                </button>
-            </div>
-        </div>
+    for($I = 0; $I < count($languages); $I+=2) {
 
-    </div>
-</form></div></div></div>
-<!-- END FORM-->';
+        echo '<div class="form-body"><div class="form-group last"><label class="col-md-2"></label><label class="col-md-5" align="CENTER">' . $languages[$I] . '</label>';
+        if($I+1 < count($languages)){ echo '<label class="col-md-5" align="CENTER">' . $languages[$I + 1] . '</label>'; }
+        echo '</div></div>';
+
+        $Field = getFieldname("title", $languages[$I]);
+
+        echo '<div class="form-body"><div class="form-group last"><label class="control-label col-md-2">' . $Title . ' Title</label>';
+        echo '<div class="col-md-5"><input class="form-control" name="' . $Field . '" id="' . $Field . '-' . $name . '" value="' . ucfirst($cms->$Field) . '"/></div>';
+        if($I+1 < count($languages)) {
+            $Field = getFieldname("title", $languages[$I+1]);
+            echo '<div class="col-md-5"><input class="form-control" name="' . $Field . '" id="' . $Field . '-' . $name . '" value="' . ucfirst($cms->$Field) . '"/></div>';
+        }
+        echo '</div></div>';
+
+        $Field = getFieldname("desc", $languages[$I]);
+        echo '<div class="form-body"><div class="form-group last"><label class="control-label col-md-2">Description</label><div class="col-md-5">';
+        echo '<textarea class="ckeditor form-control" name="' . $Field . '" rows="6" id="' . $Field . $name . '">' . $cms->$Field . '</textarea></div>';
+        if($I+1 < count($languages)) {
+            $Field = getFieldname("desc", $languages[$I + 1]);
+            echo '<div class="col-md-5"><textarea class="ckeditor form-control" name="' . $Field . '" rows="6" id="' . $Field . $name . '">' . $cms->$Field . '</textarea></div>';
+        }
+        echo '</div></div>';
+    }
+
+    echo '<div class="form-actions" style="margin-left: -10px;margin-right: -10px;"><div class="row" align="right"><div class="col-md-offset-2 col-md-10">';
+    echo '<button type="submit" class="btn btn-primary">Save Changes</button><button type="button" class="btn default" style="margin-right: 8px;">Cancel</button>';
+    echo '</div></div></div></form></div></div></div><!-- END FORM-->';
+
     return "";
 }
 //<button type="submit"   class="btn btn-primary" onclick="savepage(' . "'" . $name . "'" . ');" >
@@ -80,7 +56,7 @@ foreach($pages as $key => $value){
 echo '</ul><div class="portlet-body"><div class="tab-content">';
 $class = "active";
 foreach($pages as $key => $value){
-    $class = makepage($this->request->webroot, $key, $value, $this->requestAction("/pages/get_content/" . $value), $class);
+    $class = makepage($this->request->webroot, $key, $value, $this->requestAction("/pages/get_content/" . $value), $class, $languages);
 }
  ?>
                                     </div></div>
